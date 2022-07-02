03e0cf7b04658b320feaca51ec8021d56a23d528
Semyon Savkin <savkinsd2089@gmail.com>
2022-07-02 16:36:11 -0700

    Initial Commit

---
+++ CookieBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CookieBar
+//
+//  Created by Semyon Savkin on 02/07/2022.
+//  Copyright © 2022 Semyon Savkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
