a83f42fa21ea00baa644b4aa9a4991b3342e48c1
Semyon Savkin <savkinsd2089@gmail.com>
2022-07-02 17:07:34 -0700

    Simple touch bar override

---
+++ CookieBar/TouchBarIdentifiers.swift
@@ -0,0 +1,13 @@
+//
+//  TouchBarIdentifiers.swift
+//  CookieBar
+//
+//  Created by Semyon Savkin on 02/07/2022.
+//  Copyright © 2022 Semyon Savkin. All rights reserved.
+//
+
+import Cocoa
+
+extension NSTouchBarItem.Identifier {
+    static let label = NSTouchBarItem.Identifier("com.cookiedoth.CookieBar.label")
+}
---
+++ CookieBar/WindowController.swift
@@ -0,0 +1,17 @@
+//
+//  WindowController.swift
+//  CookieBar
+//
+//  Created by Semyon Savkin on 02/07/2022.
+//  Copyright © 2022 Semyon Savkin. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+
+}
